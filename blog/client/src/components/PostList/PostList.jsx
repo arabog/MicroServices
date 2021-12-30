ed1b8eeca3b678a8ba1a38d267d35622492a9b0c
arabambi <arababs2015@gmail.com>
2021-12-30 12:04:24 +0100

    post list updated

--- blog/client/src/components/PostList/PostList.jsx
+++ blog/client/src/components/PostList/PostList.jsx
@@ -1,12 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from "axios"
 
 
 const PostList = () => {
+          const [posts, setPosts] = useState({})
 
+          
+          const fetchPosts = async () => {
+                    const res = await axios.get("http://localhost:4002/posts")
+
+                    setPosts(res.data)
+          }
+
+
+          useEffect(() => {
+                    fetchPosts()
+          }, [])
+
+          
+          const renderedPosts = Object.values(posts)
+          
 
           return (
-                    <div>
-                              Post List
+                    <div className='d-flex flex-row flex-wrap justify-content-between'>
+                              {
+                                        renderedPosts.map(post => (
+                                                  <div 
+                                                            className='card'
+                                                            style={{width: "30%", marginBottom: "20px"}}
+                                                            key={post.id}
+                                                  >
+                                                            <div className="card-body">
+                                                                      <h3>{post.title}</h3>
+                                                            </div>
+                                                  </div>
+                                        ))
+                              }
                     </div>
           )
 }
